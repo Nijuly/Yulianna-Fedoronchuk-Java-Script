a64c75deb40d20c059e74bc1c1ea28c7323a7ec2
Nijuly <ul.fedoronchuk@gmail.com>
2022-06-02 12:21:15 +0300

    Added task4, added styles, correct task3

--- task3.js
+++ task3.js
@@ -2,12 +2,7 @@ function filterInputNumbers() {
     const inputNumbers = document.querySelector('.input').value;
     const arr = inputNumbers.split(',');
     const newArr = arr.filter(item => {
-        if (item == 0) {           //добавлено исключение, так как 0 кратен любому числу ив результате будет 0
-            return false;
-        }
-        else if (item % 3 === 0) {
-            return true;
-        }
+        return (item % 3 === 0) && (item > 0);
     });
     showResult(newArr);
 }
@@ -17,7 +12,7 @@ document.getElementById("startFilter").onclick = filterInputNumbers;
 function showResult(arr) {
     const result = document.createElement('p');
     result.classList.add('result');
-    if (arr == 0) {
+    if (arr.length == 0) {
         result.innerHTML = `Результат: искомые элементы отсутсвуют`;
     }
     else {
